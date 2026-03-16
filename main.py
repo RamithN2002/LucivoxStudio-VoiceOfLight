4830105c0d3bbd6433ef177ad56e3498a316d1f7
RamithN2002 <115481232+RamithN2002@users.noreply.github.com>
2026-03-17 01:25:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.upload_routes import router as upload_router
+from routes.chat_routes import router as chat_router
+from auth.auth_routes import router as auth_router
+
+app = FastAPI(title="Lucivox Studio API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
+
+app.include_router(chat_router)
+app.include_router(upload_router)
